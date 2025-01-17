0a9c02b0fe52efb1106d93b9d4d4d43f983f37ee
Hugh Brown <hughdbrown@yahoo.com>
2025-01-17 09:11:36 -0700

    Refactor

--- file_filer.py
+++ file_filer.py
@@ -2,25 +2,30 @@
 
 from pathlib import Path
 
+# directories to collection of file extensions
+INV_EXTS = {
+    "~/Books": [".pdf"],
+    "~/Photos": [".jpg", ".jpeg", ".heic"],
+    "~/Text": [".txt"],
+}
+
+# Convert to a dictionary mapping file extension to directory
+EXTS = {
+    ext: Path(dest).expanduser()
+    for dest, exts in INV_EXTS.items()
+    for ext in exts
+}
+
 
 def move_file(p: Path, dest: Path): 
     print(f'mv "{str(p)}" "{str(dest)}/."')
 
 
 def filer():
-    inv_exts = {
-        "~/Books": [".pdf"],
-        "~/Photos": [".jpg", ".jpeg", ".heic"],
-    }
-    exts = {
-        ext: Path(dest).expanduser()
-        for dest, exts in inv_exts.items()
-        for ext in exts
-    }
-
     for p in Path('.').glob('*'):
-        if p.is_file() and (destination := exts.get(p.suffix)):
+        if p.is_file() and (destination := EXTS.get(p.suffix)):
             move_file(p, destination)
+
 
 if __name__ == '__main__':
     filer()
